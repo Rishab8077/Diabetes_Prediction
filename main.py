2f098024858505bc1b4f3755a2a79162146bc188
Rishab8077 <rishabbothra56@gmail.com>
2021-09-25 15:22:58 +0545

    Adding UI

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+from flask import Flask, request, url_for, redirect, render_template
+import pickle
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+model = pickle.load(open("random_forest_model.pkl", "rb"))
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    text1 = request.form['1']
+    text2 = request.form['2']
+    text3 = request.form['3']
+    text4 = request.form['4']
+    text5 = request.form['5']
+    text6 = request.form['6']
+    text7 = request.form['7']
+    text8 = request.form['8']
+
+    row_df = pd.DataFrame([pd.Series([text1, text2, text3, text4, text5, text6, text7, text8])])
+    print(row_df)
+    prediction = model.predict_proba(row_df)
+    output = '{0:.{1}f}'.format(prediction[0][1], 2)
+    output = str(float(output) * 100) + '%'
+    if output > str(0.5):
+        return render_template('result.html',
+                               pred=f'You have chance of having diabetes.\nProbability of having Diabetes is {output}')
+    else:
+        return render_template('result.html', pred=f'You are safe.\n Probability of having diabetes is {output}')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
